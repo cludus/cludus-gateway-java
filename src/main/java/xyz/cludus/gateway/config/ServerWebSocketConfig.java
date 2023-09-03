11d628e898cf4c7e2c4e4597364c66724ab6bf2d
gilberto <gilberto.vento@gmail.com>
2023-09-03 17:12:32 -0400

    Initial websockets implementation.

---
+++ src/main/java/xyz/cludus/gateway/config/ServerWebSocketConfig.java
@@ -0,0 +1,24 @@
+package xyz.cludus.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import xyz.cludus.gateway.controllers.ServerWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class ServerWebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler(), "/websocket");
+    }
+
+    @Bean
+    public WebSocketHandler webSocketHandler() {
+        return new ServerWebSocketHandler();
+    }
+}
---
+++ src/main/java/xyz/cludus/gateway/controllers/ServerWebSocketHandler.java
@@ -0,0 +1,22 @@
+package xyz.cludus.gateway.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.util.HtmlUtils;
+
+public class ServerWebSocketHandler extends TextWebSocketHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ServerWebSocketHandler.class);
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String request = message.getPayload();
+        LOG.info("Server received: {}", request);
+
+        String response = String.format("response from server to '%s'", HtmlUtils.htmlEscape(request));
+        LOG.info("Server sends: {}", response);
+        session.sendMessage(new TextMessage(response));
+    }
+}
