993ef9b5290ed98631160e708c91a50ee6548c59
gilberto <gilberto.vento@gmail.com>
2023-09-04 13:23:56 -0400

    Adding unit tests

--- src/test/java/xyz/cludus/gateway/CludusGatewayApplicationTests.java
+++ src/test/java/xyz/cludus/gateway/CludusGatewayApplicationTests.java
@@ -1,13 +1,41 @@
 package xyz.cludus.gateway;
 
+import jakarta.websocket.ContainerProvider;
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.Session;
+import jakarta.websocket.WebSocketContainer;
+import org.apache.tomcat.websocket.pojo.PojoEndpointClient;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CludusGatewayApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	private static WebSocketContainer container;
+
+	@BeforeAll
+	public static void setup() {
+		container = ContainerProvider.getWebSocketContainer();
+	}
+
 	@Test
-	void contextLoads() {
+	public void testGetLog() throws Exception {
+		CountDownLatch cdl = new CountDownLatch(1);
+		var client = new TestWebSocketClient(cdl);
+		Session session = container.connectToServer(client,
+				URI.create("ws://127.0.0.1:" + port + "/websocket"));
+
+		session.getBasicRemote().sendText("hola");
+		cdl.await();
 	}
 
 }
