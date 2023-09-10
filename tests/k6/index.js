7abce598cd689e9cd97d565490d749e94eea72e8
gilberto <gilberto.vento@gmail.com>
2023-09-10 14:29:55 -0400

    Adding docker compose for k6 tests

--- tests/k6/index.js
+++ tests/k6/index.js
@@ -3,20 +3,23 @@ import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import http from 'k6/http';
 import ws from 'k6/ws';
 
-const baseUrl = 'ws://localhost:8080/websocket';
+const baseUrl = 'ws://cludus.xyz:8080/websocket';
 
 export const options = {
-    vus: 1000,
-    duration: '20m0s',
+    vus: 10000,
+    duration: '5m0s',
 };
 
 export default function () {
+    const timeout = Math.floor(60000 + (Math.random() * 60000));
+    console.log('timeout: ' + timeout);
+
     const res = ws.connect(baseUrl, {}, function (socket) {
         socket.on('open', () => {
             console.log('connected')
             socket.setInterval(() => {
                 socket.send(Date.now())
-            }, Math.floor(Math.random() * 10000))
+            }, timeout)
         });
         socket.on('message', (data) => console.log('Message received: ', data));
         socket.on('close', () => console.log('disconnected'));
