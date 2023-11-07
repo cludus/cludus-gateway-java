62e139e6fe35555288970002c1e0c3d46e66c0aa
gilberto <gilberto.vento@gmail.com>
2023-11-06 22:18:03 -0500

    Adding redis container for unit testing.

--- src/main/java/xyz/cludus/gateway/config/SecurityConfig.java
+++ src/main/java/xyz/cludus/gateway/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
